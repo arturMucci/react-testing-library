3c0f634d27262738e652f9b95567ffcd707f580e
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-22 18:32:28 -0300

    requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { act } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import '@testing-library/jest-dom';
+
+describe('Pagina de Pokémon Favoritos:', () => {
+  test('Se a pagina possui a mensagem de não haver pokemon favorito', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/favorites');
+    });
+
+    const noFavPokemon = getByText(/no favorite pokémon found/i);
+
+    expect(noFavPokemon).toBeInTheDocument();
+  });
+});
