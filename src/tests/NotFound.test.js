13821bb0c2bb0930ec4ab8a527e52917c0620c5d
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-22 18:40:08 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { act } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import '@testing-library/jest-dom';
+
+test('Se aparece a devida imagem', () => {
+  const { history, getByRole } = renderWithRouter(<App />);
+
+  act(() => {
+    history.push('/noutfond');
+  });
+
+  const title = getByRole('heading', { name: /page requested not found/i });
+  const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const img = getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe(imgSrc);
+  expect(title).toBeInTheDocument();
+});
